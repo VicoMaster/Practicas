99f47620610fb6e9d21c4db0740e53fb19c2d896
VicoMaster <job.devcode@gmail.com>
2022-05-19 04:55:11 -0500

    filter

--- Filter_Select/assets/simselect.js
+++ Filter_Select/assets/simselect.js
@@ -1,7 +1,8 @@
 const $SIMSELECT = document.getElementById('simselect-header'),
     $SIMSELECT_OPTIONS = document.getElementById('simselect-options'),
     $SIMSELECT_INPUT = document.getElementById('simselect-input'),
-    $CONTAINER_HIDDEN_SIMSELECTOR = document.querySelector('.simselect-click-hidden');
+    $CONTAINER_HIDDEN_SIMSELECTOR = document.querySelector('.simselect-click-hidden'),
+    $SIMSELECT_FILTER = document.getElementById('simselect-filter');
 
 function simselectShow() {
     $SIMSELECT_OPTIONS.classList.toggle('simselect-hidden');
@@ -9,6 +10,7 @@ function simselectShow() {
         $SIMSELECT_ICON_DOWN = document.getElementById('simselect-icon-down');
     $SIMSELECT_ICON_DOWN.classList.toggle('simselect-hidden');
     $SIMSELECT_ICON_UP.classList.toggle('simselect-hidden');
+    $SIMSELECT_FILTER.focus();
 }
 
 function actionOption(event) {
@@ -19,6 +21,11 @@ function actionOption(event) {
     }
 }
 
+function test(event){
+    console.log(event.key);
+}
+
 
 $SIMSELECT.addEventListener('click', simselectShow);
 $SIMSELECT_OPTIONS.addEventListener('click', actionOption);
+//$SIMSELECT_FILTER.addEventListener('keypress', test);
