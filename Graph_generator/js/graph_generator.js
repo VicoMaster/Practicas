96c7e462afea2a578dde13412269a42521c40aa6
VicoMaster <job.devcode@gmail.com>
2023-02-09 07:15:49 -0500

    Complete 2 charts

--- Graph_generator/js/graph_generator.js
+++ Graph_generator/js/graph_generator.js
@@ -1,14 +1,33 @@
 // [HTML INJECTION]
-const $HTML_INJECTION = '<main class="graph-generator"> <header class="graph-generator__header"> <section class="graph-generator__header-section"> <div class="input-field col s12 graph-generator__select"> <select id="graphRegion"> </select> <label class="graph-generator__select-label">Región</label> </div><div class="input-field col s12 graph-generator__select"> <select multiple id="graphItems"> <option value="" selected>Todos</option> </select> <label class="graph-generator__select-label">Items</label> </div><div class="input-field col s12 graph-generator__select"> <select multiple id="graphPeriodos"> <option value="" selected>Todos</option> </select> <label class="graph-generator__select-label">Periodos</label> </div><section class="graph-generator__section-icons"> <article id="buttomBarChart" class="material-symbols-outlined bg-l-grey c-l-blue" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Gráfico de barras">bar_chart </article> <article id="buttomLineChart" class="material-symbols-outlined bg-l-grey" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Gráfico de línea">show_chart</article> <article id="tableBarChart" class="material-symbols-outlined bg-l-grey" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Tabla de datos">table_chart</article> </section> </section> <section class="graph-generator__header-section"> <div class="graph-generator__container-flex"> <article class="graph-generator__title"> <span>Titulo Principal</span> </article> <article class="material-symbols-outlined" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Pantalla completa">fullscreen</article> </div></section> </header> <section class="graph-generator__body" id="myChart"></section> <footer class="graph-generator__footer"> <section class="graph-generator__container-flex w-max-c position-relative"> <article class="material-symbols-outlined" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Cambiar Vista">visibility</article> <span class="text-abs-right">Vista #1</span> </section> <section class="graph-generator__section-icons"> <article class="material-symbols-outlined" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Imprima/Guarde el gráfico">print</article> <article class="material-symbols-outlined" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Descargue los datos">download</article> </section> </footer> </main>';
+const $HTML_INJECTION = '<main class="graph-generator"> <header class="graph-generator__header"> <section class="graph-generator__header-section"> <div class="input-field col s12 graph-generator__select"> <select id="graphRegion"> </select> <label class="graph-generator__select-label">Región</label> </div><div class="input-field col s12 graph-generator__select"> <select multiple id="graphItems"></select> <label class="graph-generator__select-label">Items</label> </div><div class="input-field col s12 graph-generator__select"> <select multiple id="graphPeriodos"></select> <label class="graph-generator__select-label">Periodos</label> </div><section class="graph-generator__section-icons"> <article id="buttomBarChart" class="material-symbols-outlined bg-l-grey c-l-blue" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Gráfico de barras">bar_chart </article> <article id="buttomLineChart" class="material-symbols-outlined bg-l-grey" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Gráfico de línea">show_chart</article> <article id="tableBarChart" class="material-symbols-outlined bg-l-grey" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Tabla de datos">table_chart</article> </section> </section> <section class="graph-generator__header-section"> <div class="graph-generator__container-flex"> <article class="graph-generator__title"> <span>Titulo Principal</span> </article> <article class="material-symbols-outlined" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Pantalla completa">fullscreen</article> </div></section> </header> <section class="graph-generator__body" id="myChart"></section> <footer class="graph-generator__footer"> <section class="graph-generator__container-flex w-max-c position-relative"> <article class="material-symbols-outlined" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Cambiar Vista">visibility</article> <span class="text-abs-right">Vista #1</span> </section> <section class="graph-generator__section-icons"> <article class="material-symbols-outlined" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Imprima/Guarde el gráfico">print</article> <article class="material-symbols-outlined" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Descargue los datos">download</article> </section> </footer> </main>';
 const $CONTAINER_GRAPH_INJECTION = document.getElementById('graphInjection');
 $CONTAINER_GRAPH_INJECTION.innerHTML = $HTML_INJECTION;
 // [VARIABLES]
-let nuevoChart = undefined;
+let graphChart = undefined;  // Instancia del Graph
+let instancesMaterializeSelects = undefined;  // Instancias de los Selects Materialize
+// [COMPONENTES HTML]
+const $PRINCIPAL_TITLE = document.querySelector('.graph-generator__title span');
 // [ACTIVAMOS TOOLTIPS DE BOOTSTRAPS]
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 // [FUNCTIONS]
+function removeChildsContainer(contenedorPadre) {
+    //Eliminamos los hijos si existen
+    while (contenedorPadre.firstChild) {
+        contenedorPadre.removeChild(contenedorPadre.firstChild);
+    }
+}
+function destroyInstances() {
+    instancesMaterializeSelects.forEach(instance => {
+        instance.destroy();
+    });
+}
+function resetInstancesMaterialize() {
+    let elems = document.querySelectorAll('select');
+    instancesMaterializeSelects = M.FormSelect.init(elems);
+}
 async function buildData(params) {
+    // retorna formato de datos para pintar en [graphChart]
     const DATA = params.data,
         ITEMS = params.items,
         REGION = params.region;
@@ -27,17 +46,54 @@ async function buildData(params) {
     });
     return dataSet;
 }
-async function fillSelect() {
-    // Llenamos los selects con datos
-    const $SELECT_REGION = document.getElementById('graphRegion');
-    const $SELECT_ITEMS = document.getElementById('graphItems');
-    const $SELECT_PERIODOS = document.getElementById('graphPeriodos');
-    const DATA = DATA_JSON;
-    let dataSet = [];
+function fillSelectItems(params) {
+    // Re llenado de [SELECT=items] y retorna set de Items
+    const DATA = params.data,
+        REGION = params.region,
+        $SELECT_ITEMS = params.select;
     let itemSet = new Set();
-    let regionSet = new Set();
+    Object.keys(DATA).forEach(element => {
+        if (DATA[element].region === REGION && !itemSet.has(DATA[element].item)) {
+            const ELEMENT = DATA[element],
+                $OPTION = document.createElement('option');
+            $OPTION.setAttribute('selected', '');
+            $OPTION.setAttribute('value', ELEMENT.item);
+            $OPTION.textContent = DATA[element].item;
+            itemSet.add(DATA[element].item);
+            $SELECT_ITEMS.appendChild($OPTION);
+        }
+    });
+    return itemSet;
+}
+function fillSelectPeriodos(params) {
+    // Re llenado de [SELECT=periodos] y retorna set de Periodos
+    const DATA = params.data,
+        REGION = params.region,
+        $SELECT_PERIODOS = params.select;
     let periodoSet = new Set();
-    const PRIMERA_REGION = DATA[0].region;
+    Object.keys(DATA).forEach(element => {
+        if (DATA[element].region === REGION && DATA[element].periodo !== '' && DATA[element].periodo !== undefined && !periodoSet.has(DATA[element].periodo)) {
+            const ELEMENT = DATA[element];
+            const $OPTION = document.createElement('option');
+            $OPTION.setAttribute('selected', '');
+            $OPTION.setAttribute('value', ELEMENT.periodo);
+            $OPTION.textContent = DATA[element].periodo;
+            periodoSet.add(DATA[element].periodo);
+            $SELECT_PERIODOS.appendChild($OPTION);
+        }
+    });
+    return periodoSet;
+}
+async function firstFillSelect() {
+    // Primer llenado (Se ejecuta 1 vez) de [SELECTS], formateo de Data  y pintado de [graphChart]
+    const $SELECT_REGION = document.getElementById('graphRegion'),
+        $SELECT_ITEMS = document.getElementById('graphItems'),
+        $SELECT_PERIODOS = document.getElementById('graphPeriodos'),
+        DATA = DATA_JSON,
+        PRIMERA_REGION = DATA[0].region;
+    let regionSet = new Set(),
+        itemSet = undefined,
+        periodoSet = undefined;
     // Select Region
     Object.keys(DATA).forEach(element => {
         if (DATA[element].region !== '' && DATA[element].region !== undefined && !regionSet.has(DATA[element].region)) {
@@ -50,111 +106,87 @@ async function fillSelect() {
         }
     });
     // Select Items
-    Object.keys(DATA).forEach(element => {
-        if (DATA[element].region === PRIMERA_REGION && !itemSet.has(DATA[element].item)) {
-            const ELEMENT = DATA[element];
-            const $OPTION = document.createElement('option');
-            $OPTION.setAttribute('value', ELEMENT.item);
-            $OPTION.setAttribute('data-period', ELEMENT.periodo);
-            $OPTION.setAttribute('data-value', ELEMENT.$);
-            $OPTION.setAttribute('data-percentage', ELEMENT['%']);
-            $OPTION.setAttribute('data-c', ELEMENT['crecimiento_%']);
-            $OPTION.textContent = DATA[element].item;
-            itemSet.add(DATA[element].item);
-            $SELECT_ITEMS.appendChild($OPTION);
-            dataSet.push({ x: ELEMENT.item, y: ELEMENT.$ });
-        }
-    });
+    itemSet = fillSelectItems({ data: DATA, region: PRIMERA_REGION, select: $SELECT_ITEMS });
     // Select Periodos
-    Object.keys(DATA).forEach(element => {
-        if (DATA[element].periodo !== '' && DATA[element].periodo !== undefined && !periodoSet.has(DATA[element].periodo)) {
-            const ELEMENT = DATA[element];
-            const $OPTION = document.createElement('option');
-            $OPTION.setAttribute('value', ELEMENT.periodo);
-            $OPTION.textContent = DATA[element].periodo;
-            periodoSet.add(DATA[element].periodo);
-            $SELECT_PERIODOS.appendChild($OPTION);
-        }
-    });
+    periodoSet = fillSelectPeriodos({ data: DATA, region: PRIMERA_REGION, select: $SELECT_PERIODOS });
     // Generamos la data inicial para los graficos (primera carga)
-    const DATA_GRAPH = await buildData({ data: DATA, items: Array.from(itemSet), region: PRIMERA_REGION });
-    const $BAR_CHART = document.getElementById('myChart');
-    const $PRINCIPAL_TITLE = document.querySelector('.graph-generator__title span');
-    $PRINCIPAL_TITLE.textContent = document.getElementById('graphRegion').value;
+    // La estructura de datos será [{label: item, data:[valueItem, ValueItem...]},{label: item, data:[valueItem, ValueItem...]}]
+    // Para categorizar los datos por Región se usara la key:labels que contendrá un array con los periodos
     const LABELS = Array.from(periodoSet);
-    nuevoChart = new MyChart({ datasets: DATA_GRAPH, parent: $BAR_CHART, titleDataSet: $SELECT_PERIODOS.value, labels: LABELS });
-    nuevoChart.create();
+    const DATA_GRAPH = await buildData({ data: DATA, items: Array.from(itemSet), region: PRIMERA_REGION });
+    const $SECTION_CHART = document.getElementById('myChart');
+    graphChart = new MyChart({ datasets: DATA_GRAPH, parent: $SECTION_CHART, labels: LABELS });
+    graphChart.create();
+    $PRINCIPAL_TITLE.textContent = `${document.getElementById('graphRegion').value} - ${graphChart.type}`;
 }
-fillSelect();  // Primer llenado [SELECTS] y [GRAPH]
-
-async function dataGraph(event) {
-    const DATA = DATA_JSON;
-    let dataSet = [];
-    const $SELECT_REGION = document.getElementById('graphRegion'),
+async function changeDataSelects(event) {
+    // Cambia los datos de los [SELECTS=items,periodos] según su trigger
+    const DATA = DATA_JSON,
+        $SELECT_REGION = document.getElementById('graphRegion'),
         $SELECT_ITEMS = document.getElementById('graphItems'),
         $SELECT_PERIODOS = document.getElementById('graphPeriodos');
-    let valueRegion = $SELECT_REGION.value;
-    debugger
+    let valueRegion = $SELECT_REGION.value,
+        itemSets = undefined,
+        periodoSet = undefined;
     switch (event.target.id) {
         case 'graphRegion':
-            Object.keys(DATA).forEach(element => {
-                if (DATA[element].region === valueRegion) {
-                    const ELEMENT = DATA[element];
-                    dataSet.push({ x: ELEMENT.item, y: ELEMENT.$ });
-                }
-            });
-            document.querySelector('.graph-generator__title span').textContent = event.target.value;
-
-
-            //Init: TEST
-            let regionSet 
-            Object.keys(DATA).forEach(element => {
-                if (DATA[element].region !== '' && DATA[element].region !== undefined && !regionSet.has(DATA[element].region)) {
-                    regionSet.add(DATA[element].region);
-                    $SELECT_REGION.appendChild($OPTION);
-                }
-            });
-            //Close: TEST
-
-
-
-
-
-            // Nuevo dataSet
-            nuevoChart.datasets = dataSet;
+            $PRINCIPAL_TITLE.textContent = valueRegion
+            $PRINCIPAL_TITLE.textContent = `${valueRegion} - ${graphChart.type}`;
+            // Destruimos instancias de [Materialize] para manipular las options
+            destroyInstances();
+            // Eliminamos las options de los [SELECTS=items, periodos] y los volvemos a llenar con datos según selección
+            removeChildsContainer($SELECT_ITEMS);
+            removeChildsContainer($SELECT_PERIODOS);
+            itemSets = Array.from(fillSelectItems({ data: DATA, region: valueRegion, select: $SELECT_ITEMS }));
+            periodoSet = fillSelectPeriodos({ data: DATA, region: valueRegion, select: $SELECT_PERIODOS });
+            // Reiniciamos todas las instancias de Materialize
+            resetInstancesMaterialize();
             break;
         case 'graphItems':
-            Object.keys(DATA).forEach(element => {
-                if ((DATA[element].item === event.target.value && DATA[element].region === valueRegion) || event.target.value === 'items') {
-                    const ELEMENT = DATA[element];
-                    dataSet.push({ x: ELEMENT.item, y: ELEMENT.$ });
-                }
-            });
-            nuevoChart.datasets = dataSet;
+            itemSets = instancesMaterializeSelects[1].getSelectedValues();
+            periodoSet = instancesMaterializeSelects[2].getSelectedValues()
             break;
-
-        default:
+        case 'graphPeriodos':
+            itemSets = instancesMaterializeSelects[1].getSelectedValues();
+            periodoSet = instancesMaterializeSelects[2].getSelectedValues()
             break;
     }
+    // Se vuelve a construir la data para el Graph
+    const DATA_GRAPH = await buildData({ data: DATA, items: itemSets, region: valueRegion });
+    graphChart.datasets = DATA_GRAPH;
+    graphChart.labels = Array.from(periodoSet);
 }
+// Ejecutamos primer llenado de options en los [SELECTS]
+firstFillSelect();
 // [EVENTOS]
 const $BUTTON_BAR_CHART = document.getElementById('buttomBarChart');
 const $BUTTON_LINE_CHART = document.getElementById('buttomLineChart');
+const $BUTTON_TABLE = document.getElementById('tableBarChart');
 $BUTTON_BAR_CHART.addEventListener('click', event => {
     $BUTTON_LINE_CHART.classList.remove('c-l-blue');
+    $BUTTON_TABLE.classList.remove('c-l-blue');
     event.target.classList.add('c-l-blue');
-    nuevoChart.type = 'bar';
+    graphChart.type = 'bar';
+    $PRINCIPAL_TITLE.textContent = `${document.getElementById('graphRegion').value} - ${graphChart.type}`;
 });
 $BUTTON_LINE_CHART.addEventListener('click', event => {
     $BUTTON_BAR_CHART.classList.remove('c-l-blue');
+    $BUTTON_TABLE.classList.remove('c-l-blue');
     event.target.classList.add('c-l-blue');
-    nuevoChart.type = 'line';
+    graphChart.type = 'line';
+    $PRINCIPAL_TITLE.textContent = `${document.getElementById('graphRegion').value} - ${graphChart.type}`;
+});
+$BUTTON_TABLE.addEventListener('click', event => {
+    $BUTTON_BAR_CHART.classList.remove('c-l-blue');
+    $BUTTON_LINE_CHART.classList.remove('c-l-blue');
+    event.target.classList.add('c-l-blue');
+    $PRINCIPAL_TITLE.textContent = `${document.getElementById('graphRegion').value} - Tabla`;
 });
 document.querySelectorAll('.graph-generator__select').forEach(select => {
-    select.addEventListener('change', dataGraph);
+    select.addEventListener('change', changeDataSelects);
 });
 // [MATERIALIZE]
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems);
+    let elems = document.querySelectorAll('select');
+    instancesMaterializeSelects = M.FormSelect.init(elems);
 });
--- Graph_generator/js/myChart.js
+++ Graph_generator/js/myChart.js
@@ -1,10 +1,21 @@
 /*
     Autor: AndrésR.Dev 21-12-2022
     Description: Construye un gráfico [Chart()] usando librería chart.js en un contenedor padre. 
-    Comments: La clase provee todos los metodos necesarios para manipular la librería.
+    Comments: La clase provee todos los metodos necesarios para manipular la librería[Chart.js] de manera simple usando 2 estructuras de datos.
     Important: Se debe inicializar la librería[chart.js] en el html antes de llamar esta clase.
-*/
 
+    Data Structure: Se puede implementar de 2 formas:
+        1. DATA_GRAPH = [{x:'item', y: itemValue}, {x:'item', y: itemValue}...] 
+        2. DATA_GRAPH = [{label: item, data:[valueItem, ValueItem, ...]},{label: item, data:[valueItem, ValueItem, ...]} ...] 
+    Use: 
+        1. Implementación simple donde [X] son los items-articulos del graph y [Y] son los valores.
+           Constructor: graphChart = new MyChart({ datasets: DATA_GRAPH, parent: $CHART_INJECTION});
+        2. Implementación para multiples items categorizados. Se deberá crear 2 estructuras de datos. Un [ARRAY] y un Array de Objects [{},{},{}...].
+           En el [ARRAY] se deberán agregar las categorías y en el Array de Objects los valores a categorizar.
+           Constructor: graphChart = new MyChart({ datasets: DATA_GRAPH, labels: LABELS, parent: $SECTION_CHART });
+    Use-Important: Para la implementación-2 la longitud del [ARRAY] debe ser la misma que la cantidad de Objetos en el Array de Objects ya que,
+           cada elemento en el [ARRAY] es igual a una categoría y cada Object dentro del Array son los items:valores a categorizar.
+*/
 class MyChart {
     constructor(params = {}) {
         try {
@@ -159,7 +170,8 @@ class MyChart {
         try {
             if (this._datasets !== undefined && this._parent !== undefined) {
                 let dataGraph = {};
-                // Validamos si el dataSet se construye apartir de ejes [X-Y] o por Objetos: { x: ELEMENT.item, y: ELEMENT.$ }
+                // Validamos si el dataSet se construye apartir de ejes [X-Y] data: [{x: 'Sales', y: 20}, {x: 'Revenue', y: 10}]
+                // o por Objetos: [{label: item, data:[valueItem, ValueItem...]},{label: item, data:[valueItem, ValueItem...]}]
                 if (Object.keys(this._datasets[0]).indexOf('x') !== -1 && Object.keys(this._datasets[0]).indexOf('y') !== -1) {
                     dataGraph = {
                         ...{
@@ -260,6 +272,7 @@ class MyChart {
         }
     }
 }
-
+// Carga de clase como objeto del documento
 if (typeof window !== 'undefined') window.MyChart = MyChart;
+// Carga de clase como modulo
 //export { MyChart };
